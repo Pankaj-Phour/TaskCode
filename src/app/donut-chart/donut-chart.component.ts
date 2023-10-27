0f80953db3f8871103658cde7f9f53513e575fa5
Pankaj Phour <pankaj.phour70@gmail.com>
2023-10-27 13:47:56 +0530

    Minor update

--- src/app/donut-chart/donut-chart.component.ts
+++ src/app/donut-chart/donut-chart.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import * as Highcharts from 'highcharts';
+import { Component, OnInit } from '@angular/core'
+import * as Highcharts from 'highcharts'
 
 @Component({
   selector: 'app-donut-chart',
@@ -7,74 +7,64 @@ import * as Highcharts from 'highcharts';
   styleUrls: ['./donut-chart.component.scss']
 })
 export class DonutChartComponent implements OnInit {
+  constructor () {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-
-    // Data retrieved from https://netmarketshare.com/
-Highcharts.chart('donutcontainer', {
-  chart: {
-      plotBackgroundColor: null,
-      plotBorderWidth: 0,
-      plotShadow: false,
-      margin: [0, 0, 0, 0],
+  ngOnInit (): void {
+    Highcharts.chart('donutcontainer', {
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: 0,
+        plotShadow: false,
+        margin: [0, 0, 0, 0],
         spacingTop: 0,
         spacingBottom: 0,
         spacingLeft: 0,
         spacingRight: 0,
-        height: '60%',
-        // width: '100%'
-  },
-  credits:{
-    enabled:false
-  },
-  title: {
-      text: '240',
-      align: 'center',
-      verticalAlign: 'middle',
-      y: 30,
-  },
-  tooltip: {
-      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-  },
-  accessibility: {
-      point: {
+        height: '60%'
+      },
+      credits: {
+        enabled: false
+      },
+      title: {
+        text: '240',
+        align: 'center',
+        verticalAlign: 'middle',
+        y: 30
+      },
+      tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      accessibility: {
+        point: {
           valueSuffix: '%'
-      }
-  },
-  plotOptions: {
-      pie: {
-        colors:[
-          'rgb(96, 29, 151)',
-          'rgba(96, 29, 151,0.1)'
-        ],
+        }
+      },
+      plotOptions: {
+        pie: {
+          colors: ['rgb(96, 29, 151)', 'rgba(96, 29, 151,0.1)'],
           dataLabels: {
-              style: {
-                  fontWeight: 'bold',
-                  color: 'white'
-              }
+            style: {
+              fontWeight: 'bold',
+              color: 'white'
+            }
           },
           startAngle: -90,
           endAngle: 90,
           center: ['50%', '75%'],
-          size: '100%',
-      }
-  },
-  series: [{
-      type: 'pie',
-      name: 'spots',
-      innerSize: '80%',
-      data: [
-          ['used', 80],
-          ['available',20]
+          size: '100%'
+        }
+      },
+      series: [
+        {
+          type: 'pie',
+          name: 'spots',
+          innerSize: '80%',
+          data: [
+            ['used', 80],
+            ['free', 20]
+          ]
+        }
       ]
-  }]
-});
-
+    })
   }
-
-
-  
-
 }
