1f0e23c353257eee5c6cfe977cddfd298ff4ee61
Pankaj Phour <pankaj.phour70@gmail.com>
2023-10-27 16:41:02 +0530

    Pagination code updated

--- src/app/workflow/workflow.component.ts
+++ src/app/workflow/workflow.component.ts
@@ -11,8 +11,13 @@ export class WorkflowComponent implements OnInit {
 currentSlide:number = 1;
 groupCheck:boolean = false;
   users:any = [
-    {checked:false,name:'Pankaj Phour',userId:'@pankaj',status:'active',email:70,role:'Frontend developer',teams:['Angular','Frontend','developers'],options:['edit','delete']}
+    {checked:false,name:'Pankaj Phour',userId:'@pankaj',status:'active',email:70,role:'Frontend developer',teams:['Angular','Frontend','developers'],options:['edit','delete'],index:1}
   ]
+  randomData:any = {
+    names:['Loki Odinson','Tony Stark','Steve Rogers','Bruce Banner','Nick Fury','Steven Strange','Barry Allen','Natasha Romanoff','Thor Odinson','Bucky Barnes','Clint Barton'],
+    roles:['Backend developer','Frontend developer','Data analyst','Quality analyst','Project manager','HR Manager'],
+    teams:[['Angular','Frontend','developers'],['Node','Backend','Developer'],['management','oberver'],['Reporting','Assistance'],['Security','Hardware','Other']]
+  }
 
   icons:any = [
     'https://www.freeiconspng.com/thumbs/edit-icon-png/edit-editor-pen-pencil-write-icon--4.png',
@@ -21,9 +26,14 @@ groupCheck:boolean = false;
   constructor(private api:ApiService) { }
   ngOnInit(): void {
     for(let i=1;i<100;i++){
-      const obj= this.users[this.users.length-1];
+      const obj= {...this.users[this.users.length-1]};
       
       obj.email = Math.round(Math.random()*100);
+      obj.name = this.randomData.names[Math.floor(Math.random()*this.randomData.names.length)];
+      obj.role = this.randomData.roles[Math.floor(Math.random()*this.randomData.roles.length)];
+      obj.teams = this.randomData.teams[Math.floor(Math.random()*this.randomData.teams.length)];
+      obj.userId = `@${obj.name.split(' ')[0]}`;
+      obj.index = i+1;
       // obj.email = 84;
       obj.status = i%2 == 0 ? 'Customer' : 'Churned';
       console.log(obj,obj.email);
@@ -56,11 +66,15 @@ groupCheck:boolean = false;
     this.groupCheck = !this.groupCheck;
     let length = this.users.length;
     for(let i=0;i<length;i++){
-      if(i/10<this.currentSlide && i/10>(this.currentSlide-1)){
+      if(i/10<this.currentSlide && i/10>=(this.currentSlide-1)){
         this.users[i].checked = this.groupCheck;
         
       }
     }
+  }
+
+  togglePage(index:number){
+    this.currentSlide = index;
   }
 
 }
